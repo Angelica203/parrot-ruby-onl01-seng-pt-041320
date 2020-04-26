f71ebc9cf41c02f8cb17e111e829c79698112014
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 04:52:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -8,3 +8,15 @@ end
 
 parrot("Hello world")
 parrot()
+
+
+
+
+def is_greater_than_10(num)
+  if num > 10
+    return "its greater than 10!"
+end 
+"#{num} is not greater than 10"
+end
+
+is_greater_than_10(11)
